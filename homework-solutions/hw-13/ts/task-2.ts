42915d201d3a24e506da1a846e0877f4f1ec65cc
NataliaKondr <kondrateva861216@gmail.com>
2025-09-14 22:18:48 +0300

    Hw-13 completed task-1,task-2

---
+++ homework-solutions/hw-13/ts/task-2.ts
@@ -0,0 +1,27 @@
+// Создайте функцию validatePassword, которая принимает строку (пароль) и возвращает true, 
+// если пароль соответствует следующим правилам:
+//   - Пароль должен содержать хотя бы одну заглавную букву.
+//   - Пароль должен содержать хотя бы одну букву в нижнем регистре.
+//   - Пароль должен содержать хотя бы одну цифру.
+//   - Пароль должен быть не менее 8 символов.
+//   - Пароль не должен состоять из одних пробелов
+// Функция должна возвращать false, если хотя бы одно из условий не выполнено.
+
+
+function validatePassword(password: string): boolean {
+    if (password.trim().length === 0) return false;
+    if (password.length < 8) return false;
+
+    const hasUpper = /[A-ZА-ЯЁ]/.test(password);
+    const hasLower = /[a-zа-яё]/.test(password);
+    const hasDigit = /[0-9]/.test(password);
+    const spaceInPassword = password.trim() !== '';
+    return hasUpper && hasLower && hasDigit;
+}
+   
+console.log(validatePassword("Papapapa123")); 
+console.log(validatePassword("papapapa"));    
+console.log(validatePassword("PAPAPAPA"));    
+console.log(validatePassword("Pass1"));       
+console.log(validatePassword("        "));    
+console.log(validatePassword("Пароль123"));   
